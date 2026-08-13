469626cd75be949ecb3e1dacefa61ae2ab9aff75
demarett <thomasdemaret@free.fr>
2026-08-13 23:21:55 +0200

    refactor: make player pages reloadable by route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,5 +3,4 @@ import { HomeComponent } from './pages/home.component';
 import { ProfileComponent } from './pages/profile.component';
 import { HistoryComponent } from './pages/history.component';
 import { HelpComponent } from './pages/help.component';
-export const routes:Routes=[{path:'',component:HomeComponent},{path:'profil',component:ProfileComponent},{path:'historique',component:HistoryComponent},{path:'aide',component:HelpComponent},{path:'**',redirectTo:''}];
-
+export const routes:Routes=[{path:'',component:HomeComponent},{path:'profil/:battleTag',component:ProfileComponent},{path:'historique/:battleTag',component:HistoryComponent},{path:'aide',component:HelpComponent},{path:'**',redirectTo:''}];
